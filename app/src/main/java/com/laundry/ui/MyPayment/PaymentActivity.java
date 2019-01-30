6c99c2b5e59c5c9d078d48e8239c2978efb8dac6
askonlinesolutions.com <developertest62@gmail.com>
2019-01-30 19:14:29 +0530

    Changes by aayushi 30/1/2019 07:14 PM

--- app/src/main/java/com/laundry/ui/MyPayment/PaymentActivity.java
+++ app/src/main/java/com/laundry/ui/MyPayment/PaymentActivity.java
@@ -1,24 +1,42 @@
 package com.laundry.ui.MyPayment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
 import com.laundry.R;
+import com.laundry.ui.LoginScreen.ForgotPasswordActivity;
+import com.laundry.ui.LoginScreen.MainActivity;
 
 public class PaymentActivity extends AppCompatActivity implements View.OnClickListener {
- TextView login_title;
+ TextView login_title,placeOrder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
         login_title =findViewById(R.id.login_title);
+        placeOrder=findViewById(R.id.activity_place_order_btn);
         login_title.setOnClickListener(this);
+        placeOrder.setOnClickListener(this);
+        placeorder();
     }
 
     @Override
     public void onClick(View v) {
-        onBackPressed();
+        switch (v.getId()) {
+            case R.id.login_title:
+                onBackPressed();
+                break;
+            case R.id.activity_place_order_btn:
+                Intent i = new Intent(PaymentActivity.this, PaymentMethodActivity.class);
+                startActivity(i);
+        }
+
+    }
+    private void placeorder() {
+
+
     }
 }
---
+++ app/src/main/java/com/laundry/ui/ThankuActivity.java
@@ -0,0 +1,36 @@
+package com.laundry.ui;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.laundry.R;
+import com.laundry.ui.DryCleaner.DryCleanerActivity;
+
+public class ThankuActivity extends AppCompatActivity {
+    TextView login_title;
+    private static final long TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thanku);
+        login_title=findViewById(R.id.login_title);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(ThankuActivity.this,DryCleanerActivity.class));
+            }
+        },TIME_OUT);
+
+        login_title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+}
