f955db166e7e1ede2cf280b0b615f45f4ffb1314
askonlinesolutions <info@askonlinesolutions.com>
2019-02-01 11:13:41 +0530

    commited by Akhilesh on 01-02-2019  at 11:13 AM

---
+++ app/src/main/java/com/laundry/ui/currentLocation/CurrentLocationMapActivity.java
@@ -0,0 +1,15 @@
+package com.laundry.ui.currentLocation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.laundry.R;
+
+public class CurrentLocationMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_location_map);
+    }
+}
